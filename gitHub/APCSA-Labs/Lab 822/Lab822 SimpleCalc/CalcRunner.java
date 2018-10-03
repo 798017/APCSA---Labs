5510af20fa3b5a2f52e84a5291606ad6ce6ce36f
Keya <798017@seq.org>
2018-10-03 10:35:05 -0700

    Added folders

---
+++ gitHub/APCSA-Labs/Lab 822/Lab822 SimpleCalc/CalcRunner.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class CalcRunner here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class CalcRunner
+{
+    public static void main (){
+    SimpleCalc sc = new SimpleCalc();
+    System.out.println(sc.add(5,4));
+    
+    }
+}
+
+
+
---
+++ gitHub/APCSA-Labs/Lab816/Student.java
@@ -0,0 +1,34 @@
+
+/**
+ * Write a description of class Student here.
+ * 
+ * @author (Keya Gupta) 
+ * @version (8-16-18)
+ */
+public class Student
+{
+    // instance variables of 
+    private int studNumber;
+
+    /**
+     * Constructor for objects of class Student
+     */
+    public Student()
+    {
+        // initialise instance variables
+        studNumber = 0;
+    }
+
+    /**
+     * Student Methods
+     */
+    public void setStudNumber(int sn)
+    {
+        studNumber= sn;
+    }
+    public int getStudNumber()
+    {
+        // put your code here
+        return studNumber;
+    }
+}
